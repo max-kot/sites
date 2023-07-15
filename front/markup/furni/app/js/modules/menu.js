cdbb0b4a4814c57172ae5a29c13f427bc87c0e82
sim-max <max.kotlarenko@gmail.com>
2023-07-15 08:33:03 +0300

    adapted section product and why-us

--- front/markup/furni/app/js/modules/menu.js
+++ front/markup/furni/app/js/modules/menu.js
@@ -1,8 +1,17 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuNav = document.querySelector('.menu');
+const allLinksMenu = document.querySelectorAll('.menu a');
 
 menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
 	menuNav.classList.toggle('active');
 	document.body.classList.toggle('no-scroll');
+})
+
+allLinksMenu.forEach(function (link) {
+	link.addEventListener('click', function () {
+		menuBtn.classList.remove('active');
+		menuNav.classList.remove('active');
+		document.body.classList.remove('no-scroll');
+	})
 })
